cf60ad2fecd9ca20dfee61e6e7789c396131d6b3
German Sayago <german.sayago@gmail.com>
2025-05-06 17:18:50 -0300

    implementamos los datos y corregimos un error de index en la galeria

--- app/ui/SuccessCase/GallerySlider.jsx
+++ app/ui/SuccessCase/GallerySlider.jsx
@@ -1,9 +1,14 @@
 import { Carousel } from "react-bootstrap";
 
-export default function GallerySlider({ images }) {
+export default function GallerySlider({ images, activeIndex, onSelect }) {
   return (
     <div className="success-cases-gallery">
-      <Carousel fade interval={4000}>
+      <Carousel
+        fade
+        interval={4000}
+        activeIndex={activeIndex}
+        onSelect={onSelect}
+      >
         {images.map((src, index) => (
           <Carousel.Item key={index}>
             <img
